e0eb65b167a51eb55b469bff3d988ca579984821
Yoshikazu ITO <aisyu1976@gmail.com>
2025-01-04 13:55:54 +0900

    fizzbuzz with match

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,52 @@
+// fn fizzbuzz(end:i32){
+//     let mut x = 1;
 
-fn fizzbuzz(end:i32){
-    let mut x = 1;
+//     while x <= end{
+//         if x % 3 == 0 && x % 5 == 0{
+//             println!("FizzBuzz");
+//         } else if x % 3 ==0 {
+//             println!("Fizz");
+//         } else if x % 5 == 0 {
+//             println!("Buzz");
+//         } else {
+//             println!("{}",x);
+//         }
+//         x += 1
+//     }
 
-    while x <= end{
-        if x % 3 == 0 && x % 5 == 0{
-            println!("FizzBuzz");
-        } else if x % 3 ==0 {
-            println!("Fizz");
-        } else if x % 5 == 0 {
-            println!("Buzz");
-        } else {
-            println!("{}",x);
+//     }
+
+// fn fizzbuzz(end: i32) {
+//     let r = 1..=end;
+
+//     for x in r {
+//         if x % 3 == 0 && x % 5 == 0 {
+//             println!("FizzBuzz");
+//         } else if x % 3 == 0 {
+//             println!("Fizz");
+//         } else if x % 5 == 0 {
+//             println!("Buzz");
+//         } else {
+//             println!("{}", x);
+//         }
+      
+//     }
+// }
+
+fn fizzbuzz(end: i32) {
+    let r = 1..=end;
+
+    for x in r {
+        match x % 15 {
+            0 => println!("FizzBuzz"),
+            3 | 6 | 9 | 12 => println!("Fizz"),
+            5 | 10 => println!("Buzz"),
+            _ => println!("{}",x),
         }
-        x += 1
+      
     }
-
-    }
-    
-
-
-
+}
 
 fn main() {
-    fizzbuzz(30);
+    fizzbuzz(100);
 }
